1018d6afb0a0483d9ac1f391dc25c47224f03ec8
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-02-17 10:36:14 +0530

    feat : inv Dash
    Co-authored-by: Rhon <Rhogneld@users.noreply.github.com>

---
+++ app/components/FaqBox.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function FaqBox() {
+    const [height, setheight] = useState(0)
+  return (
+    <div className='h-[100px] overflow-hidden '></div>
+  )
+}
+
+export default FaqBox
