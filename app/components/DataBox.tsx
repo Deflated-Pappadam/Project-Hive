26ed4bf4802e656af438880d28e5dfababf53e10
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-02-17 13:36:48 +0530

    fix: responsive

--- app/components/DataBox.tsx
+++ app/components/DataBox.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function DataBox(props:Props) {
   return (
-    <div className='flex flex-col justify-center items-center w-[350px] h-[350px] bg-[#2e2f33]  rounded-xl p-5 m-2'>
+    <div className='flex flex-col justify-center items-center md:w-[350px] md:h-[350px] bg-[#2e2f33]  rounded-xl p-5 m-2 shrink-0'>
         <img src= {props.img} className='w-[150px]'></img>
         <div className='text-white poppins-regular text-3xl text-center p-4'>
        {props.text}
--- app/components/FaqBox.tsx
+++ app/components/FaqBox.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 function FaqBox() {
     const [height, setheight] = useState(0)
   return (
-    <div className='h-[100px] overflow-hidden '></div>
+    <div className='h-[100px] overflow-hidden bg-[#2e2f33] '></div>
   )
 }
 
