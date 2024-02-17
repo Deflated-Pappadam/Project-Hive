d679be28e3b7d9f722c6894d3a0a0888fa1003ba
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-02-18 02:55:09 +0530

    update: Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,10 +4,10 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="md:w-[80%] mx-auto flex justify-between items-center text-[#2e2f33] bg-[#fcf9f5] p-5 rounded-xl ">
-      <a href="/" className="md:min-w-[300px] ">
+      <div className="md:min-w-[300px] ">
         <img alt="" src="logo.png " className="w-[50px]" />
-      </a>
-      <h1 className="protest-strike-regular text-4xl">Project Hive</h1>
+      </div>
+      <a href="/"  className="protest-strike-regular text-4xl">Project Hive</a>
       <div className="flex justify-center md:min-w-[300px]">
         <a href="/abstract.pdf" download className="md:flex hidden w-fit border-[2px] border-black md:px-4 py-2 rounded-lg poppins-semibold md:text-xl text-sm ">
           Download Abstract
